de3405d285aa2c8c9c6aea0a3cf036e056635b93
devfrx <zagor2012@icloud.com>
2025-07-04 22:52:20 +0200

    style: Clean up formatting and remove unnecessary comments in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,26 @@ import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 import { resolve } from "path";
 
-// https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(), // Aggiunge Vue DevTools integrati
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["vue", "vue-router"],
+          lucide: ["lucide-vue-next"],
+          ionic: ["@ionic/vue", "ionicons"],
+        },
+      },
+    },
+  },
+  base: "./",
 });
